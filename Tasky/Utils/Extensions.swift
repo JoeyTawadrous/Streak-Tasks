b07ece566d9ac0020368119d96e1aa47e051fadf
Joey <joeytawadrous@gmail.com>
2018-09-07 17:04:33 +0100

    Fixed ipad size issue

--- Tasky/Utils/Extensions.swift
+++ Tasky/Utils/Extensions.swift
@@ -122,37 +122,3 @@ extension String {
 		return self.substring(to: firstIndex).capitalized + self.substring(from: firstIndex).lowercased()
 	}
 }
-
-
-extension UIView {
-	func setHeight(height: CGFloat) {
-		var frame: CGRect = self.frame
-		frame.size.height = height
-		self.frame = frame
-	}
-	
-	func setWidth(width: CGFloat) {
-		var frame: CGRect = self.frame
-		frame.size.width = width
-		self.frame = frame
-	}
-	
-	func addBorderTop(size: CGFloat, color: UIColor) {
-		addBorderUtility(x: 0, y: 0, width: frame.width, height: size, color: color)
-	}
-	func addBorderBottom(size: CGFloat, color: UIColor) {
-		addBorderUtility(x: 0, y: frame.height - size, width: frame.width, height: size, color: color)
-	}
-	func addBorderLeft(size: CGFloat, color: UIColor) {
-		addBorderUtility(x: 0, y: 0, width: size, height: frame.height, color: color)
-	}
-	func addBorderRight(size: CGFloat, color: UIColor) {
-		addBorderUtility(x: frame.width - size, y: 0, width: size, height: frame.height, color: color)
-	}
-	private func addBorderUtility(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat, color: UIColor) {
-		let border = CALayer()
-		border.backgroundColor = color.cgColor
-		border.frame = CGRect(x: x, y: y, width: width, height: height)
-		layer.addSublayer(border)
-	}
-}

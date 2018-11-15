65725180cf7f0dc0513449ddf410fd38b294cb85
Joey <joeytawadrous@gmail.com>
2018-11-15 18:31:34 +0000

    Added repeat interval for notification

--- Tasky/Tasks.swift
+++ Tasky/Tasks.swift
@@ -94,12 +94,11 @@ class Tasks: UIViewController, UITableViewDataSource, UITableViewDelegate {
                 if (notificationUUID == taskUUID) {
                     let overdueTasks = tasks.filter({ (task) -> Bool in
                         let when = task.value(forKey: Constants.CoreData.WHEN) as! Date
-						notification.repeatInterval = NSCalendar.Unit.day
                         let dateComparisionResult: ComparisonResult = notification.fireDate!.compare(when)
-                        
                         return dateComparisionResult == ComparisonResult.orderedAscending
                     })
-                    
+					
+					notification.repeatInterval = NSCalendar.Unit.day
                     notification.applicationIconBadgeNumber = overdueTasks.count   // set new badge number
                     UIApplication.shared.scheduleLocalNotification(notification)      // reschedule notification
                 }
